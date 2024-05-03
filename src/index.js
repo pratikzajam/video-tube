66b5639141f947ca80990a7bdf443b8573e4ec10
PratikZajam <zajampratik@gmail.com>
2024-05-03 23:33:29 +0530

    commited

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from 'app.js';
 
 dotenv.config({
   path: "./env",
 });
 
+app=express();
+
 import mongoose from "mongoose";
 
-connectDB();
-
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-// app.on("error",(e)=>{
-//     console.log("ERR:",error);
-//     throw error
-// })
-
-// app.listen(process.env.PORT,()=>{
-//     console.log(`App is listening on port ${process.env.PORT}`);
-// })
-
-//   } catch (error) {
-//     console.log("ERROR", error);
-//     throw err;
-//   }
-// })();
+connectDB()
+.then(()=>{
+  app.listen(process.env.port||8000,()=>{
+    console.log(`Server is running at port:${process.env.PORT}`)
+  })  .catch((err)=>{
+    console.log("MONGO db connection failed!!",err)
+  })
+})
---
+++ src/models/video.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+},
+{
+    timestamps:true
+}    
+)
+
+
+const videoSchema = new Schema({
+  videoFile: {
+    type: String, //cloudnary,
+    required: true,
+  },
+  thumbnail: {
+    type: String, //cloudnary,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  isPublished: {
+    type: Boolean,
+    default: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
