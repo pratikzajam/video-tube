a767464267c7d3f7efb72f8826da524195a8fbd5
PratikZajam <zajampratik@gmail.com>
2024-06-10 16:29:52 +0530

    add playlist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -5,6 +5,7 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { updateVideo } from "./video.controller.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
   const { name, description, userId } = req.body;
@@ -37,22 +38,168 @@ const createPlaylist = asyncHandler(async (req, res) => {
 const getUserPlaylists = asyncHandler(async (req, res) => {
   const { userId } = req.params;
 
+  console.log(userId);
+
   if (!userId) {
     throw new ApiError(400, "All fields required");
   }
 
-  const userExists = await user.findById(userId);
+  const userExists = await User.findById(userId);
 
   if (!userExists) {
     throw new ApiError(400, "user does not exists");
   }
 
-  
+  const UserId = new mongoose.Types.ObjectId(userId);
+
+  const userPlaylist = await Playlist.aggregate([
+    //for owner of playlist
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              fullname: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+    //for videos of playlist
+    {
+      $lookup: {
+        from: "videos",
+        localField: "videos",
+        foreignField: "_id",
+        as: "videos",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              video: 1,
+              thumbnail: 1,
+              videoFile: 1,
+              title: 1,
+              views: 1,
+              owner: 1,
+            },
+          },
+          //for owner of videos
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    _id: 1,
+                    username: 1,
+                    fullname: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        videos: {
+          $first: "$videos",
+        },
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, userPlaylist, "User playlist is fetched sucessfully")
+    );
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, playlist, "Playlist fetched sucessfully"));
 });
 
 const getPlaylistById = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
-  //TODO: get playlist by id
+
+  if (!playlistId) {
+    throw new ApiError(400, "All fields required");
+  }
+
+  const playlistexists = await Playlist.findById(playlistId);
+
+  if (!playlistexists) {
+    throw new ApiError(400, "playlist does not exists");
+  }
+
+  const playlist_id = new mongoose.Types.ObjectId(playlistId);
+
+  const playlist = await Playlist.aggregate([
+    {
+      $match: {
+        _id: playlist_id,
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        description: 1,
+        videos: 1,
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "videos",
+        foreignField: "_id",
+        as: "videoDetails",
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        description: 1,
+        videoDetails: 1, // Exclude the videos field from the final output
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { playlist }, "Playlist fetched sucesssfully"));
 });
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
@@ -157,9 +304,43 @@ const deletePlaylist = asyncHandler(async (req, res) => {
 const updatePlaylist = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
   const { name, description } = req.body;
-  //TODO: update playlist
-});
 
+  if (!playlistId) {
+    throw new ApiError(400, "All fields required");
+  }
+
+  if (!name || !description) {
+    throw new ApiError(400, "All fields required");
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist) {
+    throw new ApiError(400, "Playlist not found");
+  }
+
+  const updateVideos = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $set: {
+        name: name,
+        description: description,
+      },
+    },
+    { new: true }
+  );
+
+  if (!updateVideos) {
+    throw new ApiError(
+      400,
+      "There is some issues while updating the video details"
+    );
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, "video details updated sucessfully"));
+});
 export {
   createPlaylist,
   getUserPlaylists,
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {createPlaylist,addVideoToPlaylist,deletePlaylist,removeVideoFromPlaylist} from '../controllers/playlist.controller.js';
+import {createPlaylist,addVideoToPlaylist,deletePlaylist,removeVideoFromPlaylist,getUserPlaylists,getPlaylistById,updatePlaylist} from '../controllers/playlist.controller.js';
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -7,6 +7,9 @@ const router=Router()
 
 
 router.route("/createplaylist").post(createPlaylist);
+router.route("/getuserplaylists/:userId").post(getUserPlaylists);
+router.route("/getplaylistbyid/:playlistId").post(getPlaylistById);
+router.route("/updateplaylist/:playlistId").patch(updatePlaylist);
 router.route("/deletePlaylist/:playlistId").delete(deletePlaylist);
 router.route("/addvideo/:playlistId/:videoId").post(addVideoToPlaylist);
 router.route("/removevideo/:playlistId/:videoId").post(removeVideoFromPlaylist);
